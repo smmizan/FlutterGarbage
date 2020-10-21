f3158431847265286b2833f964b20fa705dda93d
unknown <smmizancse@gmal.com>
2020-10-21 11:35:45 +0600

    connectivity

--- flutter_desktop_app_frame/lib/home_pages.dart
+++ flutter_desktop_app_frame/lib/home_pages.dart
@@ -1,3 +1,4 @@
+import 'package:cross_connectivity/cross_connectivity.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePages extends StatefulWidget {
@@ -13,12 +14,29 @@ class _MyHomePagesState extends State<MyHomePages> {
         title: Text('App Bar'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Center(
-            child: ,
+            child: ConnectivityBuilder(
+              builder: (context,isConnection,status) => Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    isConnection == true ? Icons.signal_wifi_4_bar : Icons.signal_wifi_off,
+                      color: isConnection == true ? Colors.green : Colors.red,
+                    size: 40,
+                  ),
+
+                  SizedBox(width: 10,),
+
+                  Text('$status', style: TextStyle(fontSize:30, color: status != ConnectivityStatus.none ? Colors.green : Colors.red),),
+                ],
+              ),
+            ),
           )
         ],
       ),
-    )
+    );
   }
 }

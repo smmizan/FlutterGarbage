b72982b1353eec7784cd0dc74dfa73f5fc57f571
unknown <smmizancse@gmal.com>
2020-10-21 10:52:15 +0600

    cross conection

---
+++ flutter_desktop_app_frame/lib/home_pages.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyHomePages extends StatefulWidget {
+  @override
+  _MyHomePagesState createState() => _MyHomePagesState();
+}
+
+class _MyHomePagesState extends State<MyHomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App Bar'),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: ,
+          )
+        ],
+      ),
+    )
+  }
+}

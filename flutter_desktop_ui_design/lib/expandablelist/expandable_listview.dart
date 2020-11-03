cda18ec2579e3c980c3914f0d80e8d5c02571d7d
smmizancse <smmizancse@gmail.com>
2020-11-03 23:46:59 +0600

    exp list data

--- flutter_desktop_ui_design/lib/expandablelist/expandable_listview.dart
+++ flutter_desktop_ui_design/lib/expandablelist/expandable_listview.dart
@@ -8,6 +8,80 @@ class ExpandableListview extends StatefulWidget {
 class _ExpandableListviewState extends State<ExpandableListview> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expandable Listview'),
+      ),
+      body: ListView.builder(
+        itemCount: data.length(),
+        itemBuilder: (context,index) => EntryItem(data[index]),
+      ),
+    );
   }
 }
+
+
+
+class Entry{
+  final String title;
+  final List<Entry> children;
+
+  Entry(this.title, [this.children = const <Entry>[]]);
+
+}
+
+
+
+final List<Entry> data = <Entry>[
+  Entry(
+      'Section A',
+      <Entry> [
+          Entry('Sub Section of A-1',
+            <Entry>[
+              Entry('Item A-1'),
+              Entry('Item A-2'),
+              Entry('Item A-3'),
+            ]
+
+          ),
+        Entry('Sub Section of A-2'),
+        Entry('Sub Section of A-3'),
+
+
+      ],
+
+
+  ),
+
+  Entry(
+      'Section B',
+        <Entry>[
+          Entry('Sub Section B-1'),
+          Entry('Sub Section B-2'),
+        ]
+
+  ),
+
+
+  Entry(
+    'Section C',
+    <Entry>[
+      Entry('sub section c 0'),
+      Entry('sub section c 1'),
+      Entry('sub section c 2',
+        <Entry> [
+            Entry('child sub section c-2-1'),
+            Entry('child sub section c-2-2'),
+            Entry('child sub section c-2-3'),
+        ]
+      ),
+
+    ]
+  )
+
+];
+
+
+
+
+

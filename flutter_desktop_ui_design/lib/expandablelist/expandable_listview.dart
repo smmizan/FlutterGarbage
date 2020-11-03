2c42404fe79181601c230bda32436dbbcd57a0fd
smmizancse <smmizancse@gmail.com>
2020-11-03 23:22:37 +0600

    expandable list

---
+++ flutter_desktop_ui_design/lib/expandablelist/expandable_listview.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExpandableListview extends StatefulWidget {
+  @override
+  _ExpandableListviewState createState() => _ExpandableListviewState();
+}
+
+class _ExpandableListviewState extends State<ExpandableListview> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
